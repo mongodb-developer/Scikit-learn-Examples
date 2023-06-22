1edb4af3f42a1e93643e2a20a08a4f73188ccffd
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-06-21 19:02:37 -0500

    Update basic.py
    We demonstrate two basic data analysis tasks:
    
    Printing Summary Statistics: Using the describe() function of pandas DataFrame, we print summary statistics of the data, such as count, mean, standard deviation, minimum, 25th percentile, median, 75th percentile, and maximum values for each column.
    
    Printing Data Head: Using the head() function of pandas DataFrame, we print the first few rows of the DataFrame to inspect the structure and content of the data.
    
    Feel free to modify the script according to your specific MongoDB configuration, the structure of your data, and the data analysis tasks you want to perform using pandas.

--- Pandas/basic.py
+++ Pandas/basic.py
@@ -1,1 +1,23 @@
+from pymongo import MongoClient
+import pandas as pd
 
+# Connect to the MongoDB server
+client = MongoClient('<connection_string>')  # Replace <connection_string> with your actual connection string
+
+# Access the desired database and collection
+db = client['your_database']  # Replace 'your_database' with the name of your database
+collection = db['your_collection']  # Replace 'your_collection' with the name of your collection
+
+# Fetch data from MongoDB collection
+data = []
+for document in collection.find():
+    data.append(document)
+
+# Create a pandas DataFrame
+df = pd.DataFrame(data)
+
+# Perform data analysis with pandas
+print("Summary Statistics:")
+print(df.describe())
+print("\nData Head:")
+print(df.head())
